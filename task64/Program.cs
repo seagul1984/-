b390e6fae7fc176b36fe96a04aed37810219bf6f
evakoroleva <sigal1984@mail.ru>
2023-10-28 12:59:29 +0300

    Написана задача 66

---
+++ task64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int GetNumber(string message)
+{
+    int result;
+    while (true)
+    {
+        System.Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+            break;
+        else System.Console.WriteLine("Введено ненатуральное число. Задайте натуральное число :");
+    }
+    return result;
+}
+string AddNumbers(int n)
+{
+    if (n == 1)
+        return n.ToString();
+    return n + ", " + AddNumbers(n - 1);
+}
+Console.Clear();
+int n = GetNumber("Введите число N: ");
+Console.WriteLine ($" Натуральные числа в промежутке от N до 1 : "+ AddNumbers (n));
---
+++ task66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetNumber(string message)
+{
+    int result;
+    while (true)
+    {
+        System.Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+            break;
+        else System.Console.WriteLine("Введено ненатуральное число. Задайте натуральное число :");
+    }
+    return result;
+}
+
+int GetSum(int m, int n)
+{
+    int sum = 0;
+    if (m == n)
+        return n;
+    if (m < n)
+        return sum += (m + GetSum(m + 1, n));
+    return 0;
+}
+Console.Clear();
+int m = GetNumber($"Введите число М: ");
+int n = GetNumber($"Введите число N: ");
+System.Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N {m} до {n}:  {GetSum(m, n)}");
